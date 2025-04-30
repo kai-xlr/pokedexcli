d38ebff28ded7316cb02a2d5b08943c908341ae7
kai-xlr <xhauma@protonmail.com>
2025-04-30 09:33:57 -0400

    Working on the map command

--- command_map.go
+++ command_map.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+var locationAPI string = "https://pokeapi.co/api/v2/location/"
 
 func commandMap() error {
-	fmt.Println("")
+	res, err := http.Get(locationAPI)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var locate []string
+	if err = json.Unmarshal(body, &locate); err != nil {
+		return nil
+	}
+	fmt.Printf("%s", body)
+	fmt.Printf("%s", locate)
+
 	return nil
 }
