5547be67cc158ce64897536709ba2980af432ff1
kai-xlr <xhauma@protonmail.com>
2025-05-07 11:26:30 -0400

    Completed caching

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -12,23 +12,52 @@ type cacheEntry struct {
 
 type Cache struct {
 	entries map[string]cacheEntry
-	mutex   sync.Mutex
+	mutex   *sync.Mutex
 }
 
-func NewCache(interval time.Duration) *Cache {
-	cache := &Cache{
+func NewCache(interval time.Duration) Cache {
+	c := Cache{
 		entries: make(map[string]cacheEntry),
+		mutex:   &sync.Mutex{},
 	}
-	return cache
+	go c.reaploop(interval)
+	return c
 }
 
 func (c *Cache) Add(key string, val []byte) {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
-
-	entry := cacheEntry{
+	c.entries[key] = cacheEntry{
 		createdAt: time.Now(),
 		val:       val,
 	}
-	c.entries[key] = entry
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	entry, ok := c.entries[key]
+	if !ok {
+		return nil, false
+	}
+	return entry.val, true
+}
+
+func (c *Cache) reaploop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+
+	go func() {
+		for {
+			<-ticker.C
+			c.mutex.Lock()
+
+			now := time.Now()
+			for key, entry := range c.entries {
+				if now.Sub(entry.createdAt) > interval {
+					delete(c.entries, key)
+				}
+			}
+			c.mutex.Unlock()
+		}
+	}()
 }
